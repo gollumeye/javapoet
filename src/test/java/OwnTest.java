3d216dc78ef78ffb89a190b1d1f12d71e2bb4e24
Laura Randl <gollumeye@outlook.de>
2024-10-24 08:32:47 +0200

    first test run

---
+++ src/test/java/OwnTest.java
@@ -0,0 +1,24 @@
+import com.squareup.javapoet.*;
+import javax.lang.model.element.Modifier;
+import java.io.IOException;
+
+public class OwnTest {
+    public static void main(String[] args) throws IOException {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello World")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+        javaFile.writeTo(System.out);
+    }
+}
