ff97b14262497b05c75eda28a3e05ebc8d5b2a69
Laura Randl <gollumeye@outlook.de>
2025-01-04 15:10:50 +0100

    extracted nested multiset class from CodeWriter.java

--- src/main/java/com/squareup/javapoet/CodeWriter.java
+++ src/main/java/com/squareup/javapoet/CodeWriter.java
@@ -522,26 +522,4 @@ final class CodeWriter {
     result.keySet().removeAll(referencedNames);
     return result;
   }
-
-  // A makeshift multi-set implementation
-  private static final class Multiset<T> {
-    private final Map<T, Integer> map = new LinkedHashMap<>();
-
-    void add(T t) {
-      int count = map.getOrDefault(t, 0);
-      map.put(t, count + 1);
-    }
-
-    void remove(T t) {
-      int count = map.getOrDefault(t, 0);
-      if (count == 0) {
-        throw new IllegalStateException(t + " is not in the multiset");
-      }
-      map.put(t, count - 1);
-    }
-
-    boolean contains(T t) {
-      return map.getOrDefault(t, 0) > 0;
-    }
-  }
 }
