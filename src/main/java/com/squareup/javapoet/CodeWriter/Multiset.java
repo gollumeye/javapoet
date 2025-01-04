3f3ac79298f53deac9d4f8a3712624e481ceb649
Laura Randl <gollumeye@outlook.de>
2025-01-04 14:35:29 +0100

    extracted multiset class

---
+++ src/main/java/com/squareup/javapoet/CodeWriter/Multiset.java
@@ -0,0 +1,28 @@
+package com.squareup.javapoet.CodeWriter;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// A makeshift multi-set implementation
+public class Multiset<T> {
+    private final Map<T, Integer> map = new LinkedHashMap<>();
+
+    void add(T t) {
+        int count = map.getOrDefault(t, 0);
+        map.put(t, count + 1);
+    }
+
+    void remove(T t) {
+        int count = map.getOrDefault(t, 0);
+        if (count == 0) {
+            throw new IllegalStateException(t + " is not in the multiset");
+        }
+        map.put(t, count - 1);
+    }
+
+    boolean contains(T t) {
+        return map.getOrDefault(t, 0) > 0;
+    }
+}
+
